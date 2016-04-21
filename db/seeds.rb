294712b9aeec161381e0c2bacf0f080b6328d370
Zaneh Williams <zanehmw@gmail.com>
2016-04-21 16:13:34 -0400

    chose better pictures

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 Entry.destroy_all
 
-entry1 = Entry.create(author: "Zaneh", photo_url: "hongdae.png", date_taken: "10/08/2015")
-entry2 = Entry.create(author: "Zaneh", photo_url: "cuba.png", date_taken: "10/08/2015")
-entry3 = Entry.create(author: "Hewan", photo_url: "icecream.png", date_taken: "10/08/2015")
-entry4 = Entry.create(author: "Hewan", photo_url: "pizza.png", date_taken: "10/08/2015")
-entry5 = Entry.create(author: "Habby", photo_url: "puppy2.png", date_taken: "10/08/2015")
-entry6 = Entry.create(author: "Habby", photo_url: "puppy1.png", date_taken: "10/08/2015")
+entry1 = Entry.create(author: "Zaneh", photo_url: "http://boundforbusan.com/wp-content/uploads/2015/01/Hongdae-1.jpg", date_taken: "10/08/2015")
+entry2 = Entry.create(author: "Zaneh", photo_url: "http://static6.businessinsider.com/image/551cfb9deab8eac30bc2b3a3-1200-924/street-scene-with-cuban-people-and-colorful-old-buildings-in-havana-6.jpg", date_taken: "10/08/2015")
+entry3 = Entry.create(author: "Hewan", photo_url: "http://www.bonfireadventures.com/new/wp-content/uploads/2015/03/tropical-vacation-in-Bali.jpg", date_taken: "10/08/2015")
+entry4 = Entry.create(author: "Hewan", photo_url: "http://static.independent.co.uk/s3fs-public/styles/article_large/public/thumbnails/image/2016/03/04/10/seoul-getty.jpg", date_taken: "10/08/2015")
+entry5 = Entry.create(author: "Habby", photo_url: "http://cdn.belize.com/wp-content/uploads/2012/11/aerial-belize-city-swing-bridge-600.jpg", date_taken: "10/08/2015")
+entry6 = Entry.create(author: "Habby", photo_url: "http://travels.kilroy.net/media/8556953/korea-seoul-cityview.jpg", date_taken: "10/08/2015")
