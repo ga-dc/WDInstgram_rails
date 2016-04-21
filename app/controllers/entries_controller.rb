15fb11c2170c95cfdabc85f02d5800d08a8f4d6b
Wes Reed <wesley.a.reed@gmail.com>
2016-04-21 12:22:35 -0400

    Add show entry functionality

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,9 +1,20 @@
 class EntriesController < ApplicationController
 
+  def show
+    @entry = Entry.find(params[:id])
+  end
+
   def new
   end
 
   def create
-    render plain: params[:entry].inspect
+    @entry = Entry.new(entry_params)
+    @entry.save
+    redirect_to @entry
   end
+
+  private
+    def entry_params
+      params.require(:entry).permit(:author, :photo_url, :date_taken)
+    end
 end
