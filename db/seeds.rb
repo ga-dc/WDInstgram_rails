491cbd3e06da3a77f645d86e2d1654aa1b057899
Lauren C. Kennedy <laurenckennedy@gmail.com>
2016-04-21 16:03:40 -0400

    crud functionality

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Entry.destroy_all
+
+one = Entry.create(author:"Lauren", photo_url: "http://s8.favim.com/610/72/Favim.com-sunny-blue.-beach-hand-instagram-683112.jpg")
+two = Entry.create(author:"Lauren", photo_url: "https://s-media-cache-ak0.pinimg.com/736x/cd/d4/a8/cdd4a897e4bbb3e0665f330520e123aa.jpg")
+three = Entry.create(author:"Lauren", photo_url: "http://cdn.onegreenplanet.org/wp-content/uploads/2010/10//2014/10/Screen-Shot-2014-10-21-at-12.41.25-PM.png")
